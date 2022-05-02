29b3a6b13ec73e610b51309301c5b7359d9bc884
Evan Cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-05-02 23:13:49 +0000

    HTML, JS, AND CSS FIXES, ADDED FAVICON.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Created by: Evan Cagiannos
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML/",
+  });
+}
+
+/**
+ * This function determines if a person is eligible for movie rates.
+ */
+function myButtonClicked() {
+  //input
+  const seventeenAbove = document.getElementById("seventeenAbove").checked
+  const thirteenToSixteen = document.getElementById("thirteenToSixteen").checked
+  const fiveToTwelve = document.getElementById("fiveToTwelve").checked
+  //process
+  if (seventeenAbove == true) {
+    document.getElementById("answers").innerHTML = "You are eligible to watch R rated movies alone."
+  } else if (thirteenToSixteen == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch PG 13 rated movies alone."
+  } else if (fiveToTwelve == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch G and PG rated movies alone."
+  } else {
+      document.getElementById("answers").innerHTML = "You are most likely not eligible to watch any rated movies alone."
+  }
+}
